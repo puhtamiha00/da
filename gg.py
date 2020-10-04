47621c2e4566a502cecce6b736ee8cae7465e6a2
puhtamiha00 <71961265+puhtamiha00@users.noreply.github.com>
2020-10-04 04:03:55 -0500

    Add files via upload

---
+++ gg.py
@@ -0,0 +1,15 @@
+def sumFun(a,b,c):
+    sum=a,b,c
+    print("Определим сумму a=",a, ",b=",b,",c+",c,"равное",sum)
+sumFun(1,2,3)
+sumFun(3,5,78)
+x=int(input("Введите значение числа"))
+y=int(input("Введите значение числа"))
+z=int(input("Введите значение числа"))
+
+def prFun(*pAR):
+    count=0
+        if(i%2==0)
+            count+=1
+            pr*=i
+    return count,pr
